71866002187ae6ff00e0e89589324d5cced41324
Josh-dotcom-prog <edyangujoshua@gmail.com>
2025-04-04 13:58:24 +0300

    user schemas and response modals

---
+++ app/schemas/users.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel,constr, conint
+import enum
+
+class UserRole(enum.Enum):
+    MANUFACTURER = "manufacturer"
+    ADMIN = "admin"
+
+class UserCreateSchema(BaseModel):
+    name: str
+    email: str
+    mobile: conint(ge=1000000000, le=999999999999999)  # Ensures 10-15 digits
+    role: UserRole
+    password: constr(min_length=8)
+
+
+class ActivateUserSchema(BaseModel):
+    email: str
+    token: str
+
+class UserLoginSchema(BaseModel):
+    username: str
+    password: str
+
+class UserForgotPasswordSchema(BaseModel):
+    email: str
+
+class UserRestPasswordSchema(BaseModel):
+    email: str
+    token: str
+    password: constr(min_length=8)
+
+
