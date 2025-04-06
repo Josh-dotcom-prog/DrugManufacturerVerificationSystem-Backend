ac6b8a87b848d849802b4d255346a6031cb7fd26
paulebil <ebilpaul06@gmail.com>
2025-04-06 14:51:47 +0300

    created working manufacturer update service and route

--- app/services/manufacture.py
+++ app/services/manufacture.py
@@ -64,3 +64,47 @@ class ManufacturerService:
         )
 
         return manufacture_response
+
+    async def update_manufacturer(self, current_user: User, data: ManufacturerUpdate) -> ManufacturerResponse:
+        # check if user is verified
+        if not current_user.is_active:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User is not active therefore can not be a manufacturer." )
+
+        # check if manufacturer already exists
+        manufacturer_to_update = self.manufacturer_repository.get_manufacturer_by_user_id(current_user.id)
+        if not manufacturer_to_update:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Manufacturer with this id does not  exists.")
+
+        # proceed to update manufacturer
+
+        if data.name is not None:
+            manufacturer_to_update.name = data.name
+        if data.address is not None:
+            manufacturer_to_update.address = data.address
+        if data.license_file is not None:
+            manufacturer_to_update.license_file = data.license_file
+        if data.certificate_file is not None:
+            manufacturer_to_update.certificate_file = data.certificate_file
+        if data.contact_email is not None:
+            manufacturer_to_update.contact_email = data.contact_email
+        if data.contact_phone is not None:
+            manufacturer_to_update.contact_phone = data.contact_phone
+        if data.license_number is not None:
+            manufacturer_to_update.license_number = data.license_number
+
+        self.manufacturer_repository.update_manufacturer(manufacturer_to_update)
+
+        return ManufacturerResponse(
+            id=manufacturer_to_update.id,
+            name=manufacturer_to_update.name,
+            license_number=manufacturer_to_update.license_number,
+            address=manufacturer_to_update.address,
+            contact_email=manufacturer_to_update.contact_email,
+            contact_phone=manufacturer_to_update.contact_phone,
+            license_file=manufacturer_to_update.license_file,
+            certificate_file=manufacturer_to_update.certificate_file,
+
+            created_at=manufacturer_to_update.created_at,
+            updated_at=manufacturer_to_update.updated_at
+        )
+
